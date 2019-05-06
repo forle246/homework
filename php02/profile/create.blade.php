4598010423b431b71c568fb132d5535808ff5ef4
”forle246” <forle24.will@gmail.com>
2019-05-06 14:44:04 +0000

    PHP12 課題提出

---
+++ php02/profile/create.blade.php
@@ -0,0 +1,17 @@
+{{-- layouts/profile.blade.phpを読み込む --}}
+@extends('layouts.profile')
+
+
+{{-- profile.blade.phpの@yield('title')に'MY プロフィール'を埋め込む --}}
+@section('title', 'MY プロフィール')
+
+{{-- profile.blade.phpの@yield('content')に以下のタグを埋め込む --}}
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8 mx-auto">
+                <h2>MY プロフィール</h2>
+            </div>
+        </div>
+    </div>
+@endsection
